bcb100dad22ef2f01f399679a2835477c3e4d4f3
Geofrey Ernest <geofreyernest@live.com>
2025-10-22 00:58:58 +0300

    storage: build index

--- storage/view.go
+++ storage/view.go
@@ -6,11 +6,6 @@ import (
 	"errors"
 	"fmt"
 	"iter"
-	"runtime"
-	"slices"
-	"strings"
-	"sync"
-	"sync/atomic"
 
 	"github.com/gernest/roaring"
 	"github.com/gernest/roaring/shardwidth"
@@ -18,7 +13,6 @@ import (
 	"github.com/gernest/u128/checksum"
 	"github.com/gernest/u128/rbf"
 	"github.com/gernest/u128/storage/buffer"
-	"github.com/gernest/u128/storage/keys"
 	"github.com/gernest/u128/storage/magic"
 	"github.com/gernest/u128/storage/tsid"
 	"github.com/prometheus/prometheus/model/labels"
@@ -70,12 +64,7 @@ func (db *dbView) AllocateID(size uint64) (hi uint64, err error) {
 	return
 }
 
-// GetTSID generates or returns existing TSID from storage.
-//
-// This uses a single bolt database as the underlying storage. We rely heavily on
-// nested buckets to map key hierarchy.
-// Here is a simple outlook for generating tsid for metrics labels {foo=xxx, bar=yyy, baz=zzz}
-// we use a = b to denote key value paris.
+// GetTSID creates or returns existing TSID from storage.
 //
 //	                                                     ┌──────┐
 //	                                                     │ sys  │
@@ -209,169 +198,73 @@ func (db *dbView) Apply(data iter.Seq2[string, *roaring.Bitmap]) error {
 
 // Search finds matching rows across multiple views and fields. Computing views is left
 // to the caller, the only condition is they must be sorted in lexicographic order.
-func (db *dbView) Search(startView, endView []byte, startTs, endTs int64, selectors []*labels.Matcher) error {
-
-	// It is not ideal to keep long running read transaction to conserve memory.
-	// We instead open short lived ones to find relevant bits.
-	views := make([][]byte, 0, 4<<10)
-	err := db.findMatchingViews(startView, endView, func(view []byte) error {
-		views = append(views, bytes.Clone(view))
+func (db *dbView) Search(startTs, endTs int64, selectors []*labels.Matcher) error {
+	matchers := map[string]*roaring.Bitmap{}
+	err := db.buildIndex(selectors, func(field string, ra *roaring.Bitmap) error {
+		matchers[field] = ra
 		return nil
 	})
 	if err != nil {
-		return fmt.Errorf("searching for views %w", err)
-	}
-	if len(views) == 0 {
-		return nil
-	}
-
-	// We use timestamp data field to find data that is matching timestamp conditions.
-	// This resolves all shards and views we need to process if further filtering is needed.
-	rsl := newRowsSelector(startTs, endTs)
-
-	// process views concurrently. We generate ridiculous amount of views, since
-	// (view, shard) are independent we can process chunks safely.
-	var wg sync.WaitGroup
-
-	// distribute work among available cpu.
-	for chunks := range slices.Chunk(views, runtime.GOMAXPROCS(0)) {
-		wg.Add(1)
-		go rsl.Do(&wg, chunks)
-	}
-	wg.Done()
-
-	if err := rsl.Error(); err != nil {
 		return err
 	}
 
-	if !rsl.Any() {
-		// fast path: nothing matches the time range
-		return nil
+	for _, l := range selectors {
+		switch l.Type {
+		case labels.MatchEqual, labels.MatchRegexp:
+			if _, ok := matchers[l.Name]; !ok {
+				//fast path: we will never be able to fulfil match conditions.
+				// we know for a fact that the label values are not in our database.
+				return nil
+			}
+		}
 	}
 
-	// translation is view scoped:  we process views chunks concurrently.
-	for start, end := range chunkRange(len(rsl.views), runtime.GOMAXPROCS(0)) {
-		wg.Add(1)
-		go rsl.Read(&wg, start, end, selectors)
-	}
 	return nil
 }
 
-// Search for all observed views within the range.
-func (db *dbView) findMatchingViews(start, end []byte, cb func(view []byte) error) error {
+func (db *dbView) buildIndex(matchers []*labels.Matcher, cb func(field string, ra *roaring.Bitmap) error) error {
 	return db.meta.View(func(tx *bbolt.Tx) error {
-		b := tx.Bucket(sys)
-		cu := b.Cursor()
+		b := tx.Bucket(sys).Bucket(metrics)
+		if b != nil {
+			b = b.Bucket(search)
+		}
+		if b == nil {
+			return nil
+		}
 
-		// Buckets have nil value but non nil keys. Views are stored in buckets under sys.
-		// We iterate within the provided range.
-		// The upper bound is exclusive,
-		for k, _ := cu.Seek(start); k != nil && bytes.Compare(k, end) < 0; k, _ = cu.Next() {
-			err := cb(k)
-			if err != nil {
-				return err
+		for _, l := range matchers {
+			sb := b.Bucket(magic.Slice(l.Name))
+			if sb == nil {
+				continue
+			}
+			switch l.Type {
+			case labels.MatchEqual, labels.MatchNotEqual:
+				v := sb.Get(magic.Slice(l.Value))
+				if v != nil {
+					err := cb(l.Name, roaring.NewBitmap(
+						binary.BigEndian.Uint64(v),
+					))
+					if err != nil {
+						return err
+					}
+				}
+			case labels.MatchRegexp, labels.MatchNotRegexp:
+				cu := sb.Cursor()
+				px := magic.Slice(l.Prefix())
+				ra := roaring.NewBitmap()
+				for k, v := cu.Seek(px); v != nil && bytes.HasPrefix(k, px); k, v = cu.Next() {
+					ra.DirectAdd(
+						binary.BigEndian.Uint64(v),
+					)
+				}
+				err := cb(l.Name, ra)
+				if err != nil {
+					return err
+				}
 			}
 		}
 		return nil
 	})
-}
-
-type rowsSelector struct {
-	err       atomic.Value
-	db        *rbf.DB
-	views     [][]byte
-	data      [][]shardMatch
-	predicate int64
-	end       int64
-	mu        sync.Mutex
-}
-
-func newRowsSelector(predicate, end int64) *rowsSelector {
-	return &rowsSelector{
-		predicate: predicate,
-		end:       end,
-	}
-}
-
-type shardMatch struct {
-	ra    *roaring.Bitmap
-	shard uint64
-}
-
-func (t *rowsSelector) Any() bool {
-	return len(t.views) != 0
-}
-
-func (t *rowsSelector) Error() error {
-	err := t.err.Load()
-	if err != nil {
-		return err.(error)
-	}
-	return nil
-}
-
-func (t *rowsSelector) Read(wg *sync.WaitGroup, start, end int, selectors []*labels.Matcher) {
-	defer wg.Done()
-}
-
-func (t *rowsSelector) Do(wg *sync.WaitGroup, views [][]byte) {
-	defer wg.Done()
-	// If any process failed, we invalidate the whole pipeline.
-	if err := t.err.Load(); err != nil {
-		return
-	}
-	tx, err := t.db.Begin(false)
-	if err != nil {
-		t.err.Store(fmt.Errorf("opening read transaction %w", err))
-		return
-	}
-	defer tx.Rollback()
-
-	records, err := tx.RootRecords()
-	if err != nil {
-		t.err.Store(fmt.Errorf("reading root records%w", err))
-		return
-	}
-	it := records.Iterator()
-
-	b := bytesPool.Get()
-	defer bytesPool.Put(b)
-
-	for _, view := range views {
-		prefix := keys.KeyViewPrefix(b, keys.DataIndex, keys.MetricsTimestamp, view)
-		prefixText := magic.String(prefix)
-		it.Seek(prefixText)
-		base := make([]shardMatch, 0, 16)
-		for !it.Done() {
-			name, pgno, ok := it.Next()
-			if !ok {
-				break
-			}
-			if !strings.HasPrefix(name, prefixText) {
-				break
-			}
-			shard := keys.GetShard(name)
-			ra, err := readBSIRange(tx, pgno, shard, t.predicate, t.end)
-			if err != nil {
-				t.err.Store(fmt.Errorf("range search on predicates s%w", err))
-				return
-			}
-			if ra.Any() {
-				// found a match: ra is memory mapped to the current transaction.
-				/// we need to clone to avoid segfault.
-				base = append(base, shardMatch{
-					shard: shard,
-					ra:    ra.Clone(),
-				})
-			}
-		}
-		if len(base) > 0 {
-			t.mu.Lock()
-			t.views = append(t.views, view)
-			t.data = append(t.data, base)
-			t.mu.Unlock()
-		}
-	}
 }
 
 // readBSIRange performs a range search  in predicate...end bounds with upper bound being exclusive.
@@ -386,16 +279,4 @@ func readBSIRange(tx *rbf.Tx, root uint32, shard uint64, predicate, end int64) (
 	}
 	depth := mx / shardwidth.ShardWidth
 	return bitmaps.Range(cu, bitmaps.BETWEEN, shard, depth, predicate, end)
-}
-
-// chunkRange pro of slices.Chunk modified to generate indices only.
-func chunkRange(size, n int) iter.Seq2[int, int] {
-	return func(yield func(int, int) bool) {
-		for i := 0; i < size; i += n {
-			end := min(n, size-1)
-			if !yield(i, i+end) {
-				return
-			}
-		}
-	}
 }
