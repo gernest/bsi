48d78399425fcc7cdbe03edc309d128c74523c5f
Geofrey Ernest <geofreyernest@live.com>
2025-10-24 01:18:11 +0300

    storage: keys: remove unused routines

--- storage/keys/keys.go
+++ storage/keys/keys.go
@@ -1,15 +1,12 @@
 package keys
 
 import (
-	"encoding/binary"
 	"fmt"
 	"slices"
 	"strconv"
 
 	"github.com/gernest/u128/checksum"
 	"github.com/gernest/u128/storage/buffer"
-	"github.com/gernest/u128/storage/magic"
-	"github.com/gernest/u128/storage/prefix"
 )
 
 // common keys used in storage.
@@ -34,38 +31,4 @@ func View(b *buffer.B, year, week int) []byte {
 	b.B[4] = '0' + byte(week/10)
 	b.B[5] = '0' + byte(week%10)
 	return b.B
-}
-
-// Key constructs  a key we use for bitmaps in rbf storage.
-func Key(w *buffer.B, index, field []byte, shard uint64) []byte {
-	w.B = w.B[:0]
-
-	// Index is encoded as length prefix
-	w.B = prefix.Encode(w.B, index)
-	// Field is encoded as length prefixed
-	w.B = prefix.Encode(w.B, field)
-
-	// we use last 8 bytes as shard without length prefix. We use big endian to make
-	// sure the strings are sorted.
-	w.B = binary.BigEndian.AppendUint64(w.B, shard)
-	return w.B
-}
-
-func KeyViewPrefix(w *buffer.B, index, field, view []byte) []byte {
-	w.B = w.B[:0]
-
-	// Index is encoded as length prefix
-	w.B = prefix.Encode(w.B, index)
-	// Field is encoded as length prefixed
-	w.B = prefix.Encode(w.B, field)
-
-	// View is encoded as length prefixed
-	w.B = prefix.Encode(w.B, view)
-
-	return w.B
-}
-
-func GetShard(key string) uint64 {
-	b := magic.Slice(key)
-	return binary.BigEndian.Uint64(b[len(b)-8:])
 }
