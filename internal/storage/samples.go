be7e0c66c17bfd270a172cc09715396822cf4eb8
Geofrey Ernest <geofreyernest@live.com>
2025-10-29 05:25:49 +0300

    internal/storage: move samples to separate package

--- internal/storage/samples.go
+++ internal/storage/samples.go
@@ -3,251 +3,19 @@ package storage
 import (
 	"encoding/binary"
 	"fmt"
-	"math"
-	"slices"
-	"sync"
 
 	"github.com/gernest/roaring"
 	"github.com/gernest/u128/internal/bitmaps"
 	"github.com/gernest/u128/internal/rbf"
-	"github.com/gernest/u128/internal/storage/bsi"
-	"github.com/gernest/u128/internal/storage/buffer"
 	"github.com/gernest/u128/internal/storage/keys"
 	"github.com/gernest/u128/internal/storage/magic"
+	"github.com/gernest/u128/internal/storage/samples"
 	"github.com/gernest/u128/internal/storage/views"
-	"github.com/prometheus/prometheus/model/histogram"
-	"github.com/prometheus/prometheus/model/labels"
-	"github.com/prometheus/prometheus/prompb"
 	"github.com/prometheus/prometheus/storage"
-	"github.com/prometheus/prometheus/tsdb/chunkenc"
-	"github.com/prometheus/prometheus/util/annotations"
 	"go.etcd.io/bbolt"
 )
 
 var viewsPool views.Pool
-
-var samplePool = &sync.Pool{New: func() any {
-	s := new(Samples)
-	s.Init()
-	return s
-}}
-
-// Samples is a reusable container of timeseries samples.
-type Samples struct {
-	series     map[uint64]*roaring.Bitmap
-	seriesData map[uint64][]byte
-	data       map[uint64][]byte
-	kind       bsi.BSI
-	labels     bsi.BSI
-	ts         bsi.BSI
-	values     bsi.BSI
-	ls         []uint64
-	b          buffer.B
-	ref        int32
-	active     uint64
-}
-
-// NewSamples returns new Samples instance. Call Release when done using it to free
-// up resources.
-func NewSamples() *Samples {
-	s := samplePool.Get().(*Samples)
-	s.Retain()
-	return s
-}
-
-// Release returns s to the pool.
-func (s *Samples) Release() {
-	s.ref--
-	if s.ref == 0 {
-		s.Reset()
-		samplePool.Put(s)
-	}
-}
-
-func (s *Samples) Retain() {
-	s.ref++
-}
-
-func (s *Samples) Init() {
-	s.series = make(map[uint64]*roaring.Bitmap)
-	s.seriesData = make(map[uint64][]byte)
-	s.data = make(map[uint64][]byte)
-	s.labels.Init()
-	s.ts.Init()
-	s.values.Init()
-}
-
-// Reset clears s for reuse.
-func (s *Samples) Reset() {
-	s.labels.Reset()
-	s.ts.Reset()
-	s.values.Reset()
-	clear(s.series)
-	clear(s.seriesData)
-	clear(s.data)
-	s.kind.Reset()
-	s.b.B = s.b.B[:0]
-	s.ref = 0
-	s.ls = s.ls[:0]
-}
-
-// Make builds storage.SeriesSet from samples s.  We sort series by the u128 checksum
-// to ensure consistent output during tests.
-func (s *Samples) Make() storage.SeriesSet {
-	if !s.ts.Any() {
-		return storage.EmptySeriesSet()
-	}
-	s.ls = s.ls[:0]
-	for v := range s.series {
-		s.ls = append(s.ls, v)
-	}
-	slices.Sort(s.ls)
-
-	s.Retain()
-
-	// We may have s stay in memory much longer depending on PromQL. We ensure
-	// small memory footprint is occupied until s is released.
-	s.labels.Reset() // we never use this again.
-	s.kind.Optimize()
-	s.ts.Optimize()
-	s.values.Optimize()
-	return s
-}
-
-// At implements storage.SeriesSet.
-func (s *Samples) At() storage.Series {
-	s.Retain()
-	return s
-}
-
-// Next implements storage.SeriesSet.
-func (s *Samples) Next() bool {
-	if len(s.ls) == 0 {
-		s.Release()
-		return false
-	}
-	s.active = s.ls[0]
-	s.ls = s.ls[1:]
-	return true
-}
-
-// Err implements storage.SeriesSet.
-func (s *Samples) Err() error { return nil }
-
-// Warnings implements storage.SeriesSet.
-func (s *Samples) Warnings() annotations.Annotations { return nil }
-
-// Labels implements storage.SeriesSet.
-func (s *Samples) Labels() labels.Labels {
-	return buffer.WrapLabel(s.seriesData[s.active])
-}
-
-// Iterator implements storage.SeriesSet.
-func (s *Samples) Iterator(c chunkenc.Iterator) chunkenc.Iterator {
-	if c != nil {
-		i, ok := c.(*Iter)
-		if ok {
-			i.Reset()
-			i.Init(s)
-			return i
-		}
-	}
-	i := new(Iter)
-	i.Init(s)
-	return i
-}
-
-// Iter implements chunkenc.Iterator on top of Samples.
-type Iter struct {
-	po *roaring.Iterator
-	s  *Samples
-	hs *histogram.Histogram
-	fh *histogram.FloatHistogram
-	t  int64
-	f  float64
-}
-
-// Init initializes i state.
-func (i *Iter) Init(s *Samples) {
-	s.Retain()
-	i.po = s.series[s.active].Iterator()
-}
-
-// Reset clears i for reuse.
-func (i *Iter) Reset() {
-	*i = Iter{}
-}
-
-var _ chunkenc.Iterator = (*Iter)(nil)
-
-// Next implements chunkenc.Iterator.
-func (i *Iter) Next() chunkenc.ValueType {
-	idx, eof := i.po.Next()
-	if eof {
-		i.s.Release()
-		return chunkenc.ValNone
-	}
-	ts, _ := i.s.ts.GetValue(idx)
-	i.t = int64(ts)
-	kind, ok := i.s.kind.GetValue(idx)
-	if !ok {
-		panic(fmt.Errorf("missing kind for samples at post=%d", idx))
-	}
-	switch keys.Kind(kind) {
-	case keys.Float:
-		v, _ := i.s.values.GetValue(idx)
-		i.f = math.Float64frombits(uint64(v))
-		i.hs = nil
-		i.fh = nil
-		return chunkenc.ValFloat
-	case keys.Histogram:
-		var hs prompb.Histogram
-		hs.Unmarshal(i.s.data[idx])
-		if hs.IsFloatHistogram() {
-			i.fh = hs.ToFloatHistogram()
-			i.hs = nil
-			return chunkenc.ValFloatHistogram
-		}
-		i.hs = hs.ToIntHistogram()
-		i.fh = nil
-		return chunkenc.ValHistogram
-	default:
-		panic(fmt.Errorf("unexpected kind: %v  at=%d ", kind, idx))
-	}
-}
-
-// Seek implements chunkenc.Iterator.
-func (i *Iter) Seek(t int64) chunkenc.ValueType {
-	panic("not supported")
-}
-
-// At implements chunkenc.Iterator.
-func (i *Iter) At() (int64, float64) {
-	return i.t, i.f
-}
-
-// AtHistogram implements chunkenc.Iterator.
-func (i *Iter) AtHistogram(*histogram.Histogram) (int64, *histogram.Histogram) {
-	return i.t, i.hs
-}
-
-// AtFloatHistogram implements chunkenc.Iterator.
-func (i *Iter) AtFloatHistogram(w *histogram.FloatHistogram) (int64, *histogram.FloatHistogram) {
-	if i.hs != nil {
-		return i.t, i.hs.ToFloat(w)
-	}
-	return i.t, i.fh
-}
-
-// AtT implements chunkenc.Iterator.
-func (i *Iter) AtT() int64 {
-	return i.t
-}
-
-// Err implements chunkenc.Iterator.
-func (i *Iter) Err() error {
-	return nil
-}
 
 func (db *Store) Select(start, end int64) storage.SeriesSet {
 	v, err := db.selectViews(start, end)
@@ -259,7 +27,7 @@ func (db *Store) Select(start, end int64) storage.SeriesSet {
 	if len(v.Shards) == 0 {
 		return storage.EmptySeriesSet()
 	}
-	result := NewSamples()
+	result := samples.Get()
 	defer result.Release()
 
 	err = db.readTs(result, v, start, end)
@@ -297,7 +65,7 @@ func (db *Store) selectViews(start, end int64) (vs *views.List, err error) {
 	return
 }
 
-func (db *Store) readTs(result *Samples, vs *views.List, start, end int64) error {
+func (db *Store) readTs(result *samples.Samples, vs *views.List, start, end int64) error {
 	tx, err := db.rbf.Begin(false)
 	if err != nil {
 		return err
@@ -347,7 +115,7 @@ func (db *Store) readTs(result *Samples, vs *views.List, start, end int64) error
 	return nil
 }
 
-func (db *Store) applyTranslation(result *Samples) error {
+func (db *Store) applyTranslation(result *samples.Samples) error {
 
 	var b [8]byte
 
@@ -355,52 +123,52 @@ func (db *Store) applyTranslation(result *Samples) error {
 		bu := tx.Bucket(bucket)
 		for column := range columns.RangeAll() {
 			binary.BigEndian.PutUint64(b[:], column)
-			v := result.b.Own(bu.Get(b[:]))
-			result.data[column] = v
+			v := result.Own(bu.Get(b[:]))
+			result.Data[column] = v
 		}
 	}
 
 	return db.txt.View(func(tx *bbolt.Tx) error {
-		if ra := result.kind.GetColumns(int64(keys.Histogram), nil); ra.Any() {
-			readData(tx, histogramData, result.values.Transpose(ra))
+		if ra := result.KindBSI.GetColumns(int64(keys.Histogram), nil); ra.Any() {
+			readData(tx, histogramData, result.ValuesBSI.Transpose(ra))
 		}
-		if ra := result.kind.GetColumns(int64(keys.Exemplar), nil); ra.Any() {
+		if ra := result.KindBSI.GetColumns(int64(keys.Exemplar), nil); ra.Any() {
 			readData(tx, exemplarData, ra)
 		}
-		if ra := result.kind.GetColumns(int64(keys.Metadata), nil); ra.Any() {
-			readData(tx, metaData, result.values.Transpose(ra))
+		if ra := result.KindBSI.GetColumns(int64(keys.Metadata), nil); ra.Any() {
+			readData(tx, metaData, result.ValuesBSI.Transpose(ra))
 		}
 
-		series := result.labels.AsMap(nil)
+		series := result.LabelsBSI.AsMap(nil)
 
 		ra := roaring.NewBitmap()
 
 		for k, v := range series {
-			r, ok := result.series[v]
+			r, ok := result.Series[v]
 			if !ok {
 				r = roaring.NewBitmap()
-				result.series[v] = r
+				result.Series[v] = r
 			}
 			ra.DirectAdd(v)
 			r.DirectAdd(k)
 		}
 
 		return readFromU64(tx.Bucket(metaData), ra, func(id uint64, value []byte) error {
-			result.seriesData[id] = result.b.Own(value)
+			result.SeriesData[id] = result.Own(value)
 			return nil
 		})
 	})
 
 }
 
-func readSamples(result *Samples, meta views.Meta, tx *rbf.Tx, records *rbf.Records, shard uint64, match *roaring.Bitmap) error {
+func readSamples(result *samples.Samples, meta views.Meta, tx *rbf.Tx, records *rbf.Records, shard uint64, match *roaring.Bitmap) error {
 
 	{
 		root, ok := records.Get(rbf.Key{Column: keys.MetricsTimestamp, Shard: shard})
 		if !ok {
 			return fmt.Errorf("missing timestamp root record")
 		}
-		err := readBSI(tx, root, shard, meta.TsDepth, match, &result.ts)
+		err := readBSI(tx, root, shard, meta.TsDepth, match, &result.TsBSI)
 		if err != nil {
 			return fmt.Errorf("reading timestamp %w", err)
 		}
@@ -410,7 +178,7 @@ func readSamples(result *Samples, meta views.Meta, tx *rbf.Tx, records *rbf.Reco
 		if !ok {
 			return fmt.Errorf("missing metric type root record")
 		}
-		err := readBSI(tx, root, shard, meta.KindDepth, match, &result.ts)
+		err := readBSI(tx, root, shard, meta.KindDepth, match, &result.TsBSI)
 		if err != nil {
 			return fmt.Errorf("reading metric type %w", err)
 		}
@@ -420,7 +188,7 @@ func readSamples(result *Samples, meta views.Meta, tx *rbf.Tx, records *rbf.Reco
 		if !ok {
 			return fmt.Errorf("missing labels root record")
 		}
-		err := readBSI(tx, root, shard, meta.LabelsDepth, match, &result.labels)
+		err := readBSI(tx, root, shard, meta.LabelsDepth, match, &result.LabelsBSI)
 		if err != nil {
 			return fmt.Errorf("reading labels %w", err)
 		}
@@ -430,7 +198,7 @@ func readSamples(result *Samples, meta views.Meta, tx *rbf.Tx, records *rbf.Reco
 		if !ok {
 			return fmt.Errorf("missing values root record")
 		}
-		err := readBSI(tx, root, shard, meta.ValueDepth, match, &result.values)
+		err := readBSI(tx, root, shard, meta.ValueDepth, match, &result.ValuesBSI)
 		if err != nil {
 			return fmt.Errorf("reading values %w", err)
 		}
---
+++ internal/storage/samples/samples.go
@@ -0,0 +1,244 @@
+package samples
+
+import (
+	"fmt"
+	"math"
+	"slices"
+	"sync"
+
+	"github.com/gernest/roaring"
+	"github.com/gernest/u128/internal/storage/bsi"
+	"github.com/gernest/u128/internal/storage/buffer"
+	"github.com/gernest/u128/internal/storage/keys"
+	"github.com/prometheus/prometheus/model/histogram"
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/prompb"
+	"github.com/prometheus/prometheus/storage"
+	"github.com/prometheus/prometheus/tsdb/chunkenc"
+	"github.com/prometheus/prometheus/util/annotations"
+)
+
+var samplePool = &sync.Pool{New: func() any {
+	s := new(Samples)
+	s.Init()
+	return s
+}}
+
+// Samples is a reusable container of timeseries samples.
+type Samples struct {
+	Series     map[uint64]*roaring.Bitmap
+	SeriesData map[uint64][]byte
+	Data       map[uint64][]byte
+	KindBSI    bsi.BSI
+	LabelsBSI  bsi.BSI
+	TsBSI      bsi.BSI
+	ValuesBSI  bsi.BSI
+	ls         []uint64
+	b          buffer.B
+	ref        int32
+	active     uint64
+}
+
+func Get() *Samples {
+	s := samplePool.Get().(*Samples)
+	s.Retain()
+	return s
+}
+
+func (s *Samples) Own(v []byte) []byte {
+	return s.b.Own(v)
+}
+
+// Release returns s to the pool.
+func (s *Samples) Release() {
+	s.ref--
+	if s.ref == 0 {
+		s.Reset()
+		samplePool.Put(s)
+	}
+}
+
+func (s *Samples) Retain() {
+	s.ref++
+}
+
+func (s *Samples) Init() {
+	s.Series = make(map[uint64]*roaring.Bitmap)
+	s.SeriesData = make(map[uint64][]byte)
+	s.Data = make(map[uint64][]byte)
+	s.LabelsBSI.Init()
+	s.TsBSI.Init()
+	s.ValuesBSI.Init()
+}
+
+// Reset clears s for reuse.
+func (s *Samples) Reset() {
+	s.LabelsBSI.Reset()
+	s.TsBSI.Reset()
+	s.ValuesBSI.Reset()
+	clear(s.Series)
+	clear(s.SeriesData)
+	clear(s.Data)
+	s.KindBSI.Reset()
+	s.b.B = s.b.B[:0]
+	s.ref = 0
+	s.ls = s.ls[:0]
+}
+
+// Make builds storage.SeriesSet from samples s.  We sort series by the u128 checksum
+// to ensure consistent output during tests.
+func (s *Samples) Make() storage.SeriesSet {
+	if !s.TsBSI.Any() {
+		return storage.EmptySeriesSet()
+	}
+	s.ls = s.ls[:0]
+	for v := range s.Series {
+		s.ls = append(s.ls, v)
+	}
+	slices.Sort(s.ls)
+
+	s.Retain()
+
+	// We may have s stay in memory much longer depending on PromQL. We ensure
+	// small memory footprint is occupied until s is released.
+	s.LabelsBSI.Reset() // we never use this again.
+	s.KindBSI.Optimize()
+	s.TsBSI.Optimize()
+	s.ValuesBSI.Optimize()
+	return s
+}
+
+// At implements storage.SeriesSet.
+func (s *Samples) At() storage.Series {
+	s.Retain()
+	return s
+}
+
+// Next implements storage.SeriesSet.
+func (s *Samples) Next() bool {
+	if len(s.ls) == 0 {
+		s.Release()
+		return false
+	}
+	s.active = s.ls[0]
+	s.ls = s.ls[1:]
+	return true
+}
+
+// Err implements storage.SeriesSet.
+func (s *Samples) Err() error { return nil }
+
+// Warnings implements storage.SeriesSet.
+func (s *Samples) Warnings() annotations.Annotations { return nil }
+
+// Labels implements storage.SeriesSet.
+func (s *Samples) Labels() labels.Labels {
+	return buffer.WrapLabel(s.SeriesData[s.active])
+}
+
+// Iterator implements storage.SeriesSet.
+func (s *Samples) Iterator(c chunkenc.Iterator) chunkenc.Iterator {
+	if c != nil {
+		i, ok := c.(*Iter)
+		if ok {
+			i.Reset()
+			i.Init(s)
+			return i
+		}
+	}
+	i := new(Iter)
+	i.Init(s)
+	return i
+}
+
+// Iter implements chunkenc.Iterator on top of Samples.
+type Iter struct {
+	po *roaring.Iterator
+	s  *Samples
+	hs *histogram.Histogram
+	fh *histogram.FloatHistogram
+	t  int64
+	f  float64
+}
+
+// Init initializes i state.
+func (i *Iter) Init(s *Samples) {
+	s.Retain()
+	i.po = s.Series[s.active].Iterator()
+}
+
+// Reset clears i for reuse.
+func (i *Iter) Reset() {
+	*i = Iter{}
+}
+
+var _ chunkenc.Iterator = (*Iter)(nil)
+
+// Next implements chunkenc.Iterator.
+func (i *Iter) Next() chunkenc.ValueType {
+	idx, eof := i.po.Next()
+	if eof {
+		i.s.Release()
+		return chunkenc.ValNone
+	}
+	ts, _ := i.s.TsBSI.GetValue(idx)
+	i.t = int64(ts)
+	kind, ok := i.s.KindBSI.GetValue(idx)
+	if !ok {
+		panic(fmt.Errorf("missing kind for samples at post=%d", idx))
+	}
+	switch keys.Kind(kind) {
+	case keys.Float:
+		v, _ := i.s.ValuesBSI.GetValue(idx)
+		i.f = math.Float64frombits(uint64(v))
+		i.hs = nil
+		i.fh = nil
+		return chunkenc.ValFloat
+	case keys.Histogram:
+		var hs prompb.Histogram
+		hs.Unmarshal(i.s.Data[idx])
+		if hs.IsFloatHistogram() {
+			i.fh = hs.ToFloatHistogram()
+			i.hs = nil
+			return chunkenc.ValFloatHistogram
+		}
+		i.hs = hs.ToIntHistogram()
+		i.fh = nil
+		return chunkenc.ValHistogram
+	default:
+		panic(fmt.Errorf("unexpected kind: %v  at=%d ", kind, idx))
+	}
+}
+
+// Seek implements chunkenc.Iterator.
+func (i *Iter) Seek(t int64) chunkenc.ValueType {
+	panic("not supported")
+}
+
+// At implements chunkenc.Iterator.
+func (i *Iter) At() (int64, float64) {
+	return i.t, i.f
+}
+
+// AtHistogram implements chunkenc.Iterator.
+func (i *Iter) AtHistogram(*histogram.Histogram) (int64, *histogram.Histogram) {
+	return i.t, i.hs
+}
+
+// AtFloatHistogram implements chunkenc.Iterator.
+func (i *Iter) AtFloatHistogram(w *histogram.FloatHistogram) (int64, *histogram.FloatHistogram) {
+	if i.hs != nil {
+		return i.t, i.hs.ToFloat(w)
+	}
+	return i.t, i.fh
+}
+
+// AtT implements chunkenc.Iterator.
+func (i *Iter) AtT() int64 {
+	return i.t
+}
+
+// Err implements chunkenc.Iterator.
+func (i *Iter) Err() error {
+	return nil
+}
