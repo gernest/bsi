e13964144b5b74a9c106d7e17125f64c1f1a4553
Geofrey Ernest <geofreyernest@live.com>
2025-10-31 11:14:07 +0300

    internal/checksum: document Hash

--- internal/checksum/hash.go
+++ internal/checksum/hash.go
@@ -4,7 +4,8 @@ import (
 	"github.com/cespare/xxhash/v2"
 )
 
-// Hash returns 128 bit hash.
+// Hash returns uint64 xxhash checksum of data. This is the only hash function used.
+// We try to be as close as we can with prometheus, which also uses xxhash.
 func Hash(data []byte) uint64 {
 	return xxhash.Sum64(data)
 }
