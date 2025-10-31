0e5bbdbb64eb61f8add58045d358a6192a0181b4
Geofrey Ernest <geofreyernest@live.com>
2025-10-31 04:31:53 +0300

    internal/storage/api: initial storage.Storage implementation

--- internal/storage/api/api.go
+++ internal/storage/api/api.go
@@ -15,21 +15,42 @@ import (
 	"github.com/prometheus/prometheus/prompb"
 	"github.com/prometheus/prometheus/storage"
 	"github.com/prometheus/prometheus/tsdb"
+	"github.com/prometheus/prometheus/util/annotations"
 )
 
 var rowsPool rows.Pool
 
 // API implements prometheus storage api on top of our own timeseries database.
 type API struct {
-	db *db.Store
+	db db.Store
 }
 
-var _ storage.Appendable = (*API)(nil)
+var _ storage.Storage = (*API)(nil)
+
+// Init setup databases.
+func (a *API) Init(dataPath string) error {
+	return a.db.Init(dataPath)
+}
+
+// ChunkQuerier implements storage.ChunkQueryable.
+func (a *API) ChunkQuerier(_, _ int64) (storage.ChunkQuerier, error) {
+	panic("storage.ChunkQueryable  api is not supported")
+}
+
+// StartTime implements storage.Storage.
+func (a *API) StartTime() (int64, error) {
+	return a.db.StartTime()
+}
+
+// Close implements storage.Storage .
+func (a *API) Close() error {
+	return a.db.Close()
+}
 
 // Appender implements storage.Appendable
 func (a *API) Appender(_ context.Context) storage.Appender {
 	return &appender{
-		db:  a.db,
+		db:  &a.db,
 		set: rowsPool.Get(),
 	}
 }
@@ -151,4 +172,79 @@ func (w *appender) Append(_ storage.SeriesRef, l labels.Labels, t int64, v float
 	}
 	w.set.AppendFloat(l, t, v)
 	return 0, nil
+}
+
+var _ storage.Queryable = (*API)(nil)
+
+// Querier implements storage.Queryable.
+func (a *API) Querier(mint, maxt int64) (storage.Querier, error) {
+	return &querier{lo: mint, hi: maxt, db: &a.db}, nil
+}
+
+type querier struct {
+	lo, hi int64
+	db     *db.Store
+}
+
+var _ storage.Querier = (*querier)(nil)
+
+func (q *querier) Close() error {
+	return nil
+}
+
+func (q *querier) LabelValues(ctx context.Context, name string, hints *storage.LabelHints, matchers ...*labels.Matcher) ([]string, annotations.Annotations, error) {
+	return q.db.LabelValues(ctx, name, hints, matchers...)
+}
+
+func (q *querier) LabelNames(ctx context.Context, hints *storage.LabelHints, matchers ...*labels.Matcher) ([]string, annotations.Annotations, error) {
+	return q.db.LabelNames(ctx, hints, matchers...)
+}
+
+func (q *querier) Select(ctx context.Context, sort bool, hints *storage.SelectHints, matchers ...*labels.Matcher) storage.SeriesSet {
+	if hints == nil {
+		hints = &storage.SelectHints{}
+	}
+	hints.Start = max(q.lo, hints.Start)
+	hints.End = max(q.hi, hints.End)
+	return q.db.Select(ctx, sort, hints, matchers...)
+}
+
+type exemplarQuery struct {
+	db *db.Store
+}
+
+var _ storage.ExemplarStorage = (*API)(nil)
+
+// ExemplarQuerier implements storage.ExemplarStorage.
+func (a *API) ExemplarQuerier(_ context.Context) (storage.ExemplarQuerier, error) {
+	return &exemplarQuery{db: &a.db}, nil
+}
+
+// AppendExemplar implements storage.ExemplarStorage.
+func (a *API) AppendExemplar(_ storage.SeriesRef, l labels.Labels, e exemplar.Exemplar) (storage.SeriesRef, error) {
+	l = l.WithoutEmpty()
+	if l.IsEmpty() {
+		return 0, fmt.Errorf("empty labelset: %w", tsdb.ErrInvalidSample)
+	}
+	if lbl, dup := l.HasDuplicateLabelNames(); dup {
+		return 0, fmt.Errorf(`label name "%s" is not unique: %w`, lbl, tsdb.ErrInvalidSample)
+	}
+	exe := prompb.Exemplar{
+		Labels:    prompb.FromLabels(e.Labels, nil),
+		Value:     e.Value,
+		Timestamp: e.Ts,
+	}
+	data, err := exe.Marshal()
+	if err != nil {
+		return 0, err
+	}
+	set := rowsPool.Get()
+	set.AppendExemplar(l, e.Ts, data)
+	defer rowsPool.Put(set)
+
+	return 0, a.db.AddRows(set)
+}
+
+func (s *exemplarQuery) Select(start, end int64, matchers ...[]*labels.Matcher) ([]exemplar.QueryResult, error) {
+	return s.db.SelectExemplar(start, end, matchers...)
 }
