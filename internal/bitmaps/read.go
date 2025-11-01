f0eddb0b100cca026c40e0b4793757ab61463657
Geofrey Ernest <geofreyernest@live.com>
2025-11-01 07:48:24 +0300

    all: remove unused code

--- internal/bitmaps/read.go
+++ internal/bitmaps/read.go
@@ -1,63 +1,13 @@
 package bitmaps
 
 import (
-	"io"
-
 	"github.com/gernest/roaring"
 	"github.com/gernest/roaring/shardwidth"
 )
-
-const (
-	shardVsContainerExponent = 4
-)
-
-// Wrap wraps [roaring.ContainerIterator] to implement [Cursor].
-// Used mostly in tests.
-type Wrap struct {
-	it roaring.ContainerIterator
-	mx uint64
-}
-
-// NewBitmapCursor initializes [Wrap].
-func NewBitmapCursor(ra *roaring.Bitmap) *Wrap {
-	it, _ := ra.Containers.Iterator(0)
-	return &Wrap{it: it, mx: ra.Max()}
-}
-
-var _ Cursor = (*Wrap)(nil)
-
-// Close implements [Cursor].
-func (w *Wrap) Close() {}
-
-// First implements [Cursor].
-func (w *Wrap) First() error {
-	return w.Next()
-}
-func (w *Wrap) Max() (uint64, error) {
-	return w.mx, nil
-}
-
-// Value implements [Cursor].
-func (w *Wrap) Value() (uint64, *roaring.Container) {
-	return w.it.Value()
-}
-
-// Next implements [Cursor].
-func (w *Wrap) Next() error {
-	if !w.it.Next() {
-		return io.EOF
-	}
-	return nil
-}
 
 // Row reads all column ids for rowID returning them as [roaring.Bitmap].
 func Row(ra OffsetRanger, shard, rowID uint64) (*roaring.Bitmap, error) {
 	return ra.OffsetRange(shardwidth.ShardWidth*shard,
 		shardwidth.ShardWidth*rowID,
 		shardwidth.ShardWidth*(rowID+1))
-}
-
-// Existence Reads an EXISTENCE bitmap. Only works with BSI or EXISTENCE encoded bitmaps.
-func Existence(tx OffsetRanger, shard uint64) (*roaring.Bitmap, error) {
-	return Row(tx, shard, bsiExistsBit)
 }
--- internal/bitmaps/write.go
+++ internal/bitmaps/write.go
@@ -7,33 +7,6 @@ import (
 	"github.com/gernest/roaring/shardwidth"
 )
 
-const (
-	// Row ids used for boolean fields.
-	falseRowID = uint64(0)
-	trueRowID  = uint64(1)
-
-	falseRowOffset = 0 * shardwidth.ShardWidth // fragment row 0
-	trueRowOffset  = 1 * shardwidth.ShardWidth // fragment row 1
-)
-
-// Mutex (id, value[0]), (id, value[1])  .... as MUTEX into ra.
-func Mutex(ra *roaring.Bitmap, id uint64, value ...uint64) {
-	for i := range value {
-		if value[i] == 0 {
-			continue
-		}
-		ra.DirectAdd(
-			value[i]*shardwidth.ShardWidth +
-				(id % shardwidth.ShardWidth),
-		)
-	}
-}
-
-// Exist sets existing bit for id in ra. Encodes EXISTENCE bitmaps.
-func Exist(ra *roaring.Bitmap, id uint64) {
-	ra.DirectAdd(id % shardwidth.ShardWidth)
-}
-
 // BSI encodes (id, signedValue) as bit sliced index into ra.
 func BSI(ra *roaring.Bitmap, id uint64, signedValue int64) {
 	fragmentColumn := id % shardwidth.ShardWidth
@@ -53,14 +26,5 @@ func BSI(ra *roaring.Bitmap, id uint64, signedValue int64) {
 			ra.DirectAdd(row*shardwidth.ShardWidth + fragmentColumn)
 		}
 		row++
-	}
-}
-
-func Boolean(m *roaring.Bitmap, id uint64, value bool) {
-	fragmentColumn := id % shardwidth.ShardWidth
-	if value {
-		m.DirectAdd(trueRowOffset + fragmentColumn)
-	} else {
-		m.DirectAdd(falseRowOffset + fragmentColumn)
 	}
 }
