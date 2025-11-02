5f3c0c546af4bb6e4916b290691baaa8b976e48d
Geofrey Ernest <geofreyernest@live.com>
2025-11-02 13:01:56 +0300

    chore: remove obsolete comments

--- internal/storage/tsid/tsid.go
+++ internal/storage/tsid/tsid.go
@@ -1,25 +1,17 @@
-// Package tsid defines api for working with timeseries samples. We assign unique
-// identifiers for time series that are used throughout our RBF storage.
-//
-// To speed up ingestion , once processed TSID are serialized in a way that can be
-// reused when we find the same series.
 package tsid
 
 import (
 	"sync"
 )
 
-// B is reusable buffer of ID.
 type B struct {
 	B []ID
 }
 
-// Pool pools B instances to avoid excessive allocations.
 type Pool struct {
 	pool sync.Pool
 }
 
-// Get creates anew B instance or returns existing one from the pool.
 func (p *Pool) Get() *B {
 	v := p.pool.Get()
 	if v != nil {
@@ -28,19 +20,13 @@ func (p *Pool) Get() *B {
 	return new(B)
 }
 
-// Put returns b to the pool, retaining b capacity.
 func (p *Pool) Put(b *B) {
 	b.B = b.B[:0]
 	p.pool.Put(b)
 }
 
-// ID stores assigned identifiers for time series. The first Column is for the assigned
-// uint64 for the whole series, the rest are (column, assigned_uint64_for_the_value) tuple
-// for each series label where column=label name and  assigned_uint64=label value.
 type ID []Column
 
-// Column Identifies a prometheus label (name, value) tuple as uint64, to be used
-// in RBF storage.
 type Column struct {
 	ID    uint64
 	Value uint64
