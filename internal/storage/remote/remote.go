74838a8e1b3ec46d52636200f4092d3905c247b8
Geofrey Ernest <geofreyernest@live.com>
2025-11-03 08:14:49 +0300

    internal/storage/remote: write request with checksum

--- internal/storage/remote/remote.go
+++ internal/storage/remote/remote.go
@@ -3,6 +3,7 @@ package remote
 import (
 	"math"
 
+	"github.com/gernest/bsi/internal/checksum"
 	"github.com/gernest/bsi/internal/storage/buffer"
 	"github.com/gernest/bsi/internal/storage/keys"
 	"github.com/gernest/bsi/internal/storage/rows"
@@ -15,6 +16,42 @@ func Write(rows *rows.Rows, ids *tsid.B) (w *prompb.WriteRequest) {
 	w = &prompb.WriteRequest{}
 	for i := range ids.B {
 		id := ids.B[i][0].Value
+		sx, ok := series[id]
+		if !ok {
+			w.Timeseries = append(w.Timeseries, prompb.TimeSeries{})
+			sx = &w.Timeseries[len(w.Timeseries)-1]
+			for name, value := range buffer.RangeLabels(rows.Labels[i]) {
+				sx.Labels = append(sx.Labels, prompb.Label{
+					Name:  string(name),
+					Value: string(value),
+				})
+			}
+			w.Metadata = append(w.Metadata, prompb.MetricMetadata{})
+			w.Metadata[len(w.Metadata)-1].Unmarshal(rows.Metadata[i])
+			series[id] = sx
+		}
+		switch rows.Kind[i] {
+		case keys.Float:
+			sx.Samples = append(sx.Samples, prompb.Sample{
+				Value:     math.Float64frombits(rows.Value[i]),
+				Timestamp: rows.Timestamp[i],
+			})
+		case keys.Exemplar:
+			sx.Exemplars = append(sx.Exemplars, prompb.Exemplar{})
+			sx.Exemplars[len(sx.Exemplars)-1].Unmarshal(rows.Exemplar[i])
+		case keys.Histogram, keys.FloatHistogram:
+			sx.Histograms = append(sx.Histograms, prompb.Histogram{})
+			sx.Histograms[len(sx.Histograms)-1].Unmarshal(rows.Histogram[i])
+		}
+	}
+	return
+}
+
+func WriteSum(rows *rows.Rows) (w *prompb.WriteRequest) {
+	series := map[uint64]*prompb.TimeSeries{}
+	w = &prompb.WriteRequest{}
+	for i := range rows.Labels {
+		id := checksum.Hash(rows.Labels[i])
 		sx, ok := series[id]
 		if !ok {
 			w.Timeseries = append(w.Timeseries, prompb.TimeSeries{})
