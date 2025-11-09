2e2ffa8d0c36e4eb1454dc731c8e648066b00baa
Geofrey Ernest <geofreyernest@live.com>
2025-11-09 16:45:51 +0300

    internal/storage/work: sequential work

--- internal/storage/work/work.go
+++ internal/storage/work/work.go
@@ -1,9 +1,7 @@
 package work
 
 import (
-	"errors"
 	"iter"
-	"slices"
 	"sync"
 )
 
@@ -27,40 +25,15 @@ func (w *Work[T]) Reset() *Work[T] {
 
 // Do executes f in n possible goroutines. We stop execution whn we first encounter an error.
 func (w *Work[T]) Do(n int, f func(item T) error) error {
-	if n < 1 {
-		panic("work.Do: n < 1")
-	}
-	var mu sync.RWMutex
-	var errs []error
-	do := func(a []T) {
-		mu.RLock()
-		sz := len(errs)
-		mu.RUnlock()
-		if sz > 0 {
-			return
-		}
-		for i := range a {
-			err := f(a[i])
-			if err != nil {
-				mu.Lock()
-				errs = append(errs, err)
-				mu.Unlock()
-				return
-			}
+	// we do sequential scan for now.
+	// TODO: concurrent processing once promql tests pass.
+	for i := range w.todo {
+		err := f(w.todo[i])
+		if err != nil {
+			return err
 		}
 	}
-
-	var g sync.WaitGroup
-
-	for work := range slices.Chunk(w.todo, n) {
-		g.Add(1)
-		go w.do(&g, work, do)
-	}
-	g.Wait()
-	if len(errs) == 0 {
-		return nil
-	}
-	return errors.Join(errs...)
+	return nil
 }
 
 func (w *Work[T]) do(g *sync.WaitGroup, all []T, f func([]T)) {
