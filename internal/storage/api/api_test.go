63e5e4dab8046c60d6852d652f408aa461140031
Geofrey Ernest <geofreyernest@live.com>
2025-11-09 13:34:00 +0300

    chore: aggregate test case

--- internal/storage/api/api_test.go
+++ internal/storage/api/api_test.go
@@ -1,37 +1,24 @@
 package api
 
 import (
+	_ "embed"
 	"testing"
 
 	"github.com/prometheus/prometheus/promql"
+	"github.com/prometheus/prometheus/promql/parser"
 	"github.com/prometheus/prometheus/promql/promqltest"
 	"github.com/prometheus/prometheus/storage"
 	"github.com/prometheus/prometheus/util/testutil"
 	"github.com/stretchr/testify/require"
 )
 
-const in = `load 5m
-  http_requests{job="api-server", instance="0", group="production"} 0+10x10
-  http_requests{job="api-server", instance="1", group="production"} 0+20x10
-  http_requests{job="api-server", instance="0", group="canary"}   0+30x10
-  http_requests{job="api-server", instance="1", group="canary"}   0+40x10
-  http_requests{job="app-server", instance="0", group="production"} 0+50x10
-  http_requests{job="app-server", instance="1", group="production"} 0+60x10
-  http_requests{job="app-server", instance="0", group="canary"}   0+70x10
-  http_requests{job="app-server", instance="1", group="canary"}   0+80x10
-
-load 5m
-  foo{job="api-server", instance="0", region="europe"} 0+90x10
-  foo{job="api-server"} 0+100x10
-
-# Simple sum.
-eval instant at 50m SUM BY (group) (http_requests{job="api-server"})
-  {group="canary"} 700
-  {group="production"} 300
-`
+//go:embed src.txt
+var src string
 
 func TestPromql(t *testing.T) {
-	promqltest.RunTestWithStorage(t, in, newTestEngine(t), newStorage)
+	parser.EnableExperimentalFunctions = true
+	promqltest.RunTestWithStorage(t, src, newTestEngine(t), newStorage)
+
 }
 
 func newTestEngine(t *testing.T) *promql.Engine {
--- internal/storage/work/work.go
+++ internal/storage/work/work.go
@@ -1,27 +1,15 @@
-// Copyright 2018 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package work is lifter from go src. We use this to speed up shards processing
-// when during queries.
 package work
 
 import (
 	"iter"
-	"math/rand"
+	"slices"
 	"sync"
 )
 
 // Work manages a set of work items to be executed in parallel, at most once each.
 // The items in the set must all be valid map keys.
 type Work[T any] struct {
-	f       func(T) // function to run for each item
-	running int     // total number of runners
-
-	mu      sync.Mutex
-	todo    []T       // items yet to be run
-	wait    sync.Cond // wait when todo is empty
-	waiting int       // number of runners waiting for todo
+	todo []T // items yet to be run
 }
 
 // Init initializes w with todo work items
@@ -32,67 +20,28 @@ func (w *Work[T]) Init(todo iter.Seq[T]) {
 }
 
 func (w *Work[T]) Reset() *Work[T] {
-	w.f = nil
-	w.running = 0
 	w.todo = w.todo[:0]
-	w.waiting = 0
 	return w
 }
 
-// Do runs f in parallel on items from the work set,
-// with at most n invocations of f running at a time.
-// It returns when everything added to the work set has been processed.
-// At least one item should have been added to the work set
-// before calling Do (or else Do returns immediately),
-// but it is allowed for f(item) to add new items to the set.
-// Do should only be used once on a given Work.
 func (w *Work[T]) Do(n int, f func(item T)) {
 	if n < 1 {
 		panic("work.Do: n < 1")
 	}
-	if w.running >= 1 {
-		panic("work.Do: already called Do")
-	}
 
-	w.running = n
-	w.f = f
-	w.wait.L = &w.mu
+	var g sync.WaitGroup
 
-	for i := 0; i < n-1; i++ {
-		go w.runner()
+	for work := range slices.Chunk(w.todo, n) {
+		g.Add(1)
+		go w.do(&g, work, f)
 	}
-	w.runner()
+	g.Wait()
 }
 
-// runner executes work in w until both nothing is left to do
-// and all the runners are waiting for work.
-// (Then all the runners return.)
-func (w *Work[T]) runner() {
-	for {
-		// Wait for something to do.
-		w.mu.Lock()
-		for len(w.todo) == 0 {
-			w.waiting++
-			if w.waiting == w.running {
-				// All done.
-				w.wait.Broadcast()
-				w.mu.Unlock()
-				return
-			}
-			w.wait.Wait()
-			w.waiting--
-		}
+func (w *Work[T]) do(g *sync.WaitGroup, all []T, f func(T)) {
+	defer g.Done()
 
-		// Pick something to do at random,
-		// to eliminate pathological contention
-		// in case items added at about the same time
-		// are most likely to contend.
-		i := rand.Intn(len(w.todo))
-		item := w.todo[i]
-		w.todo[i] = w.todo[len(w.todo)-1]
-		w.todo = w.todo[:len(w.todo)-1]
-		w.mu.Unlock()
-
-		w.f(item)
+	for i := range all {
+		f(all[i])
 	}
 }
